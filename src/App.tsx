149bd446b297012ea877d01e58d8a40d0f36aae2
z4nta0 <z4nta0@gmail.com>
2026-03-17 09:34:04 -0500

    Fix more TS errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+
 import Home from './components/HomPagCom/HomPagCom.tsx';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
